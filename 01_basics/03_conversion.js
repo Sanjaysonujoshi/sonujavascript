923a517550d00aecc6d2f7e5c4dd3b1aa1e3094c
Sanjaysonujoshi <115540415+Sanjaysonujoshi@users.noreply.github.com>
2024-05-13 04:11:18 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -21,6 +21,32 @@
 //"hitesh" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+
+//operations//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "sanjay"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2+2);
+console.log(1+2 +"2")
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter)
