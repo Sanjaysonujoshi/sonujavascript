82e3c332548bd7139c1e078f75dca526c7fa9dca
Sanjaysonujoshi <115540415+Sanjaysonujoshi@users.noreply.github.com>
2024-05-13 04:46:05 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+//primitive
+
+//7 types :string ,Number, Boolean, null, undefined, symbol ,BigInt
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id ===anotherId)
+
+// const bigNumber = 324343535343434n
+
+// Refernce (Non premitive)
+
+//Array,objects, Functions
+
+const heros =["sakthiman","nagraj"]
+let myobj={
+    name:"sanjay",
+    age:22,
+}
+
+ const myfunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber)
