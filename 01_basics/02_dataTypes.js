929ab794d1660ee5ae228fd8a5e8c1e91726fcde
Sanjaysonujoshi <115540415+Sanjaysonujoshi@users.noreply.github.com>
2024-05-13 03:09:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict:;//treat all js code as newer vesion"
+
+let name = "sanjay"
+let age = 18
+let isLoggedIn = false  
+
+
+//Number => 2 to power 53
+//bigint
+//string => ""
+// bollean => true/false
+//null => standalone value
+// undefined => value not assing
+//symbol => find uniqueness
+
+//object 
+console.log(typeof age);//undefined
+console.log(typeof null);//object
+console.log(typeof "sanjay");
+console.log(typeof null);//object
