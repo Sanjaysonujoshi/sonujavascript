4a1321a72184f8cef268b57cb477a5775377cc65
Sanjaysonujoshi <115540415+Sanjaysonujoshi@users.noreply.github.com>
2024-05-13 02:31:29 +0000

    varibles in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sonujoshi@GMAIL.COM"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+let accountState;
+
+// accountId = 2 not allowed
+accountEmail = "joshisonu"
+accountPassword = "21212121"
+accountCity = "begaluru"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail,accountPassword,accountCity,accountState]);
