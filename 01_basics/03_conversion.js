ddf1dcc0e68ed12781637afa61c3d926cb0e26a7
Sanjaysonujoshi <115540415+Sanjaysonujoshi@users.noreply.github.com>
2024-05-13 03:47:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,26 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) //convert value string to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33 easily  convert
+//"33abc" => NAN 
+//true => 1; flase => 0
+
+// let isloggedIn = "sanjay"
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+//"" => false
+//"hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
